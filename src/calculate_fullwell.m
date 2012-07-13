e00fad50a43a002c29aa2b42182c1774b7738a8b
Norman Saez <nsaez@alma.cl>
2012-07-13 16:24:25 -0400

    Deleting aux files

--- src/calculate_fullwell.m
+++
@@ -1,2 +0,0 @@
-function [fullwell] = calculate_fullwell(gain, bits)
-fullwell = (2^bits -1) * gain;
--- src/ccd.m
+++
@@ -1,3 +0,0 @@
-function [fits] = ccd(img_collection,img_type, temp, exp_time)
-filepath = fullfile('..',img_collection,sprintf('%s_T_%d_E_%d_0001.fits',img_type,temp,exp_time));
-fits = fitsread(filepath);
